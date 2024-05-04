a15a053356ba3bbb2c8b87e0578f384d021e2f5e
Seongah Kim <kimsa0322@naver.com>
2024-05-03 18:12:45 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 5 - Starter
+Seongah Kim
+
+1. I would use a unit test to test the "message" feature of a messaging application. Because "message" feature is the major funtion of the app and I need to make sure that the fuctionality works well before implementing any additional features.
+2. I wouldn't use a unit test to test the "max message length". Because it can be tested with other features. In addition, it is not a major feature.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,52 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voiceSelect = document.getElementById('voice-select');
+  const textToSpeakInput = document.getElementById('text-to-speak');
+  const speakButton = document.querySelector('button');
+  const faceImage = document.querySelector('#explore img');
+  const defaultImgSrc = faceImage.src;
+  const openMouthImgSrc = 'assets/images/smiling-open.png';
+
+  let synth = window.speechSynthesis;
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+    voices.forEach((voice) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.setAttribute('value', voice.name);
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  function speak() {
+    if (synth.speaking) {
+      console.error('speechSynthesis.speaking');
+      return;
+    }
+    if (textToSpeakInput.value !== '') {
+      const utterance = new SpeechSynthesisUtterance(textToSpeakInput.value);
+      utterance.onstart = () => {
+        faceImage.src = openMouthImgSrc;
+      };
+      utterance.onend = () => {
+        faceImage.src = defaultImgSrc;
+      };
+      utterance.onerror = () => {
+        faceImage.src = defaultImgSrc;
+      };
+      utterance.voice = voices.find(voice => voice.name === voiceSelect.value);
+      synth.speak(utterance);
+    }
+  }
+
+  speakButton.addEventListener('click', () => {
+    speak();
+  });
 }
